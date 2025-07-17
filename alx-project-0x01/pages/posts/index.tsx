1c6fbace8361710fc96fa385a08314c2f218f118
Samuel <samiberakih3@gmail.com>
2025-07-17 23:22:24 +0300

    Add some futhers

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,10 +1,14 @@
+import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
 
 const PostsPage: React.FC = () => (
-  <div className="p-8 space-y-4">
-    <h1 className="text-3xl font-semibold mb-4">Posts</h1>
-    <PostCard title="First Post" body="This is the body of the first post." />
-    <PostCard title="Second Post" body="This is the body of the second post." />
+  <div>
+    <Header />
+    <main className="p-8 space-y-4">
+      <h1 className="text-3xl font-semibold mb-4">Posts</h1>
+      <PostCard title="First Post" body="This is the body of the first post." />
+      <PostCard title="Second Post" body="This is the body of the second post." />
+    </main>
   </div>
 );
 
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,9 +1,13 @@
+import Header from "@/components/layout/Header";
 import Button from "@/components/common/Button";
 
 const UsersPage: React.FC = () => (
-  <div className="p-8">
-    <h1 className="text-3xl font-semibold mb-4">Users</h1>
-    <Button label="Add User" onClick={() => alert("Add user clicked!")} />
+  <div>
+    <Header />
+    <main className="p-8">
+      <h1 className="text-3xl font-semibold mb-4">Users</h1>
+      <Button label="Add User" onClick={() => alert("Add user clicked!")} />
+    </main>
   </div>
 );
 
